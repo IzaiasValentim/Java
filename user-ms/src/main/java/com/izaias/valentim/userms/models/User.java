8ef0b4310d6be32d5ac6a285615308b0efa88c6b
IzaiasValentim <izaias451@gmail.com>
2023-12-10 12:10:20 -0300

    Defined the Many to Many rel of user and role.

--- user-ms/src/main/java/com/izaias/valentim/userms/models/User.java
+++ user-ms/src/main/java/com/izaias/valentim/userms/models/User.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
@@ -20,6 +22,12 @@ public class User implements Serializable {
     private String username;
 
     private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private final Set<Role> roles = new HashSet<>();
 
     public User() {
     }
@@ -61,12 +69,18 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof User user)) return false;
         return Objects.equals(username, user.username);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(username);
