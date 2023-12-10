b39fc4fc6db074b1fe9d3676fb9aff280e694ab1
IzaiasValentim <izaias451@gmail.com>
2023-12-10 13:10:47 -0300

    Added repositories for Role and User.

---
+++ user-ms/src/main/java/com/izaias/valentim/userms/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.izaias.valentim.userms.repositories;
+
+import com.izaias.valentim.userms.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
