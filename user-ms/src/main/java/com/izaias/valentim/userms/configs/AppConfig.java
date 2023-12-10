9f23c5ee105b2d9cc53bde892d6c14cf0465ace0
IzaiasValentim <izaias451@gmail.com>
2023-12-10 16:12:51 -0300

    Addes a class of configurations for the bcrypt encoder.

---
+++ user-ms/src/main/java/com/izaias/valentim/userms/configs/AppConfig.java
@@ -0,0 +1,14 @@
+package com.izaias.valentim.userms.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
