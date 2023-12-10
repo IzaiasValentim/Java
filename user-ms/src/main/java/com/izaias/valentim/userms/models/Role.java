2e7c507601365336a3cdaae96ac29b438a20320b
IzaiasValentim <izaias451@gmail.com>
2023-12-10 12:01:11 -0300

    Added the Role entity.

---
+++ user-ms/src/main/java/com/izaias/valentim/userms/models/Role.java
@@ -0,0 +1,54 @@
+package com.izaias.valentim.userms.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Role implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role role)) return false;
+        return Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+}
