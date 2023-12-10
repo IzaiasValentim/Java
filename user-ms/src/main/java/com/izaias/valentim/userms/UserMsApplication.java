998d3c89a19533c11fc1a7d988a9bc23c8fd6da6
IzaiasValentim <izaias451@gmail.com>
2023-12-10 16:15:55 -0300

    Added the spring security dependency, and disable the init lock.

--- user-ms/src/main/java/com/izaias/valentim/userms/UserMsApplication.java
+++ user-ms/src/main/java/com/izaias/valentim/userms/UserMsApplication.java
@@ -2,10 +2,11 @@ package com.izaias.valentim.userms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class UserMsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserMsApplication.class, args);
 	}
