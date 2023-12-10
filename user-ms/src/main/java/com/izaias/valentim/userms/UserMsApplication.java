d5aff1f5fd2e7502bbe0170c895e5f62f95dcd0b
IzaiasValentim <izaias451@gmail.com>
2023-12-10 11:36:55 -0300

    Added the base ms for the user.

---
+++ user-ms/src/main/java/com/izaias/valentim/userms/UserMsApplication.java
@@ -0,0 +1,13 @@
+package com.izaias.valentim.userms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMsApplication.class, args);
+	}
+
+}
