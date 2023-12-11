04ed2ccbc24b9588ec276f76053c441ce5f59b36
IzaiasValentim <izaias451@gmail.com>
2023-12-10 22:28:06 -0300

    Create some controller layer methods to create Role, findAll Roles and Delete Role.

--- user-ms/src/main/java/com/izaias/valentim/userms/controllers/RoleController.java
+++ user-ms/src/main/java/com/izaias/valentim/userms/controllers/RoleController.java
@@ -1,27 +1,39 @@
 package com.izaias.valentim.userms.controllers;
 
 import com.izaias.valentim.userms.models.Role;
-import com.izaias.valentim.userms.services.UserService;
+import com.izaias.valentim.userms.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "roles")
 public class RoleController {
-
-    private final UserService userService;
+    private final RoleService roleService;
 
     @Autowired
-    public RoleController(UserService userService) {
-        this.userService = userService;
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
     }
-    @GetMapping(value = "getAllRoles")
-    public List<Role> getAllRoles(){
-        return userService.getAllRoles();
+
+    @GetMapping(value = "")
+    public ResponseEntity<List<Role>> getAllRoles() {
+        return ResponseEntity.ok().body(roleService.getAllRoles());
     }
+
+    @PostMapping(value = "{roleName}/")
+    public ResponseEntity<?> createRole(@PathVariable String roleName) {
+        return roleService.createNewRole(roleName);
+    }
+
+    @DeleteMapping(value = "{roleName}/")
+    public ResponseEntity<?> deleteRole(@PathVariable String roleName) {
+        if (roleService.deleteRole(roleName))
+            return ResponseEntity.ok().build();
+        return ResponseEntity.badRequest().build();
+    }
+
 
 }
