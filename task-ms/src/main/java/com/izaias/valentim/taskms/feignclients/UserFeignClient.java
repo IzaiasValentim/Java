6f3515d1af01eb6ccdf8f90667e3af2401df0c76
IzaiasValentim <izaias451@gmail.com>
2023-12-21 16:41:35 -0300

    Removed unused import.

--- task-ms/src/main/java/com/izaias/valentim/taskms/feignclients/UserFeignClient.java
+++ task-ms/src/main/java/com/izaias/valentim/taskms/feignclients/UserFeignClient.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Set;
 
