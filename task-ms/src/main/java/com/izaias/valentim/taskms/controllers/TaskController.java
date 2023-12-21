16c070414d13291768bec5df98a670af3138a7db
IzaiasValentim <izaias451@gmail.com>
2023-12-21 16:38:53 -0300

    Created a temporary route to test the feignClient.

--- task-ms/src/main/java/com/izaias/valentim/taskms/controllers/TaskController.java
+++ task-ms/src/main/java/com/izaias/valentim/taskms/controllers/TaskController.java
@@ -1,6 +1,8 @@
 package com.izaias.valentim.taskms.controllers;
 
+import com.izaias.valentim.taskms.feignclients.UserFeignClient;
 import com.izaias.valentim.taskms.models.Task;
+import com.izaias.valentim.taskms.models.feignEntities.UsernameResponseFeign;
 import com.izaias.valentim.taskms.models.modelsToRequests.Usernames;
 import com.izaias.valentim.taskms.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +10,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+
 
 @RestController
 @RequestMapping(value = "tasks")
 public class TaskController {
 
     private final TaskService taskService;
+    private final UserFeignClient userFeignClient;
+
 
     @Autowired
-    public TaskController(TaskService taskService) {
+    public TaskController(TaskService taskService, UserFeignClient userFeignClient) {
         this.taskService = taskService;
+        this.userFeignClient = userFeignClient;
     }
 
     @GetMapping(value = "")
@@ -29,14 +36,18 @@ public class TaskController {
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
         return taskService.getTaskById(id);
     }
-
+    @PostMapping("users/verify")
+    public ResponseEntity<Set<UsernameResponseFeign>> verifyListOfUsernames(@RequestBody Usernames usernames) {
+       return userFeignClient.verifyListOfUsernames(usernames);
+    }
+    
     @PostMapping(value = "/")
     public ResponseEntity<Task> createNewTask(@RequestBody Task taskToCreate) {
         return taskService.createNewTask(taskToCreate);
     }
     @PostMapping(value = "{id_task}/include/users/")
     public ResponseEntity<Task> includeUsersOnTask(@PathVariable Long id_task, @RequestBody Usernames usernames){
-        return taskService.includeUsersToTask(id_task, usernames.getUsernames());
+        return taskService.includeUsersToTask(id_task, usernames);
     }
 
 }
